adfb42b1d72cfb21e2a533ce0670465c5bcea171
ssippe-sx <samuel.sippe@steelx-dev.com>
2018-10-24 17:08:53 +1000

    Output

--- src/Frame3ddn/Solver.cs
+++ src/Frame3ddn/Solver.cs
@@ -9,6 +9,22 @@ namespace Frame3ddn
         public Output Solve(Input input)
         {
             return null;
+            
+            
+        }
+
+        // ref main.c:265
+        static int Dof(Input input) => input.Nodes.Count * 6;
+
+        /// <summary>
+        /// global stiffness matrix
+        /// </summary>
+        // ref main.c:343
+        static double[,] K(Input input)
+        {
+            var dof = Dof(input);
+            var k = new double[dof,dof];
+            return k;
         }
     }
 }
