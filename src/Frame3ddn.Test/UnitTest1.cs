c5530ca4563ddd016f94459800ef29946878111f
vincentl <vincent.lu@steelx.com.au>
2018-10-26 17:13:45 +1000

    Added code to parse input.

--- src/Frame3ddn.Test/UnitTest1.cs
+++ src/Frame3ddn.Test/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace Frame3ddn.Test
@@ -6,9 +7,11 @@ namespace Frame3ddn.Test
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void GetInputFromFile()
         {
-
+            string workspaceDir = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory().ToString()).ToString()).ToString()).ToString();
+            StreamReader sr = new StreamReader(workspaceDir + "\\TestData\\exA.3dd");
+            Input input = Input.Parse(sr);
         }
     }
 }
--- src/Frame3ddn/Input.cs
+++ src/Frame3ddn/Input.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Frame3ddn
 {
@@ -8,6 +10,13 @@ namespace Frame3ddn
         public double X;
         public double Y;
         public double Z;
+
+        public Vec3(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
     }
 
     public class Node
@@ -20,6 +29,12 @@ namespace Frame3ddn
 
         public Vec3 Position { get; }
         public double Radius { get; }
+
+        public static Node Parse(string inputString)
+        {
+            double[] data = System.Text.RegularExpressions.Regex.Split(inputString, @"\s{1,}").Select(Double.Parse).ToArray();
+            return new Node(new Vec3(data[1], data[2], data[3]), data[4]);
+        }
     }
 
     public class ReactionInput
@@ -34,6 +49,12 @@ namespace Frame3ddn
         public Vec3 Position { get; }
         public Vec3 R { get; }
         public bool IsFixed { get; }
+
+        public static ReactionInput Parse(string inputString)
+        {
+            int[] data = System.Text.RegularExpressions.Regex.Split(inputString, @"\s{1,}").Select(int.Parse).ToArray();
+            return new ReactionInput(new Vec3(data[0], data[1], data[2]), new Vec3(data[3], data[4], data[5]), data[6] != 0);
+        }
     }
 
     public class FrameElement
@@ -42,6 +63,7 @@ namespace Frame3ddn
         public int NodeIdx2 { get; }
         public double Ax { get; }
         public double Asy { get; }
+        public double Asz { get; set; }
         public double Jx { get; }
         public double Iy { get; }
         public double Iz { get; }
@@ -50,12 +72,13 @@ namespace Frame3ddn
         public double Roll { get; }
         public double Density { get; }
 
-        public FrameElement(int nodeIdx1, int nodeIdx2, double ax, double asy, double jx, double iy, double iz, double e, double g, double roll, double density)
+        public FrameElement(int nodeIdx1, int nodeIdx2, double ax, double asy, double asz, double jx, double iy, double iz, double e, double g, double roll, double density)
         {
             NodeIdx1 = nodeIdx1;
             NodeIdx2 = nodeIdx2;
             Ax = ax;
             Asy = asy;
+            Asz = asz;
             Jx = jx;
             Iy = iy;
             Iz = iz;
@@ -63,6 +86,23 @@ namespace Frame3ddn
             G = g;
             Roll = roll;
             Density = density;
+        }
+        public static FrameElement Parse(string inputString)
+        {
+            string[] data = System.Text.RegularExpressions.Regex.Split(inputString, @"\s{1,}");
+            return new FrameElement(
+                int.Parse(data[1]),
+                int.Parse(data[2]),
+                double.Parse(data[3]),
+                double.Parse(data[4]),
+                double.Parse(data[5]),
+                double.Parse(data[6]),
+                double.Parse(data[7]),
+                double.Parse(data[8]),
+                double.Parse(data[9]),
+                double.Parse(data[10]),
+                double.Parse(data[11]),
+                double.Parse(data[12]));
         }
     }
 
@@ -78,6 +118,14 @@ namespace Frame3ddn
             Load = load;
             Moment = moment;
         }
+
+        public static NodeLoad Parse(string inputString)
+        {
+            string[] data = System.Text.RegularExpressions.Regex.Split(inputString, @"\s{1,}");
+            return new NodeLoad(int.Parse(data[0]), 
+                new Vec3(double.Parse(data[1]), double.Parse(data[2]), double.Parse(data[3])),
+                new Vec3(double.Parse(data[4]), double.Parse(data[5]), double.Parse(data[6])));
+        }
     }
 
     public class UniformLoad
@@ -89,7 +137,14 @@ namespace Frame3ddn
         {
             ElementIdx = elementIdx;
             Load = load;
-        }        
+        }
+
+        public static UniformLoad Parse(string inputString)
+        {
+            string[] data = System.Text.RegularExpressions.Regex.Split(inputString, @"\s{1,}");
+            return new UniformLoad(int.Parse(data[0]),
+                new Vec3(double.Parse(data[1]), double.Parse(data[2]), double.Parse(data[3])));
+        }
     }
 
     public class TrapLoad
@@ -106,6 +161,17 @@ namespace Frame3ddn
             LocationEnd = locationEnd;
             LoadStart = loadStart;
             LoadEnd = loadEnd;
+        }
+
+        public static TrapLoad Parse(string inputString)
+        {
+            string[] data = System.Text.RegularExpressions.Regex.Split(inputString, @"\s{1,}");
+            return new TrapLoad(int.Parse(data[0]),
+                new Vec3(double.Parse(data[1]), double.Parse(data[5]), double.Parse(data[9])),
+                new Vec3(double.Parse(data[2]), double.Parse(data[6]), double.Parse(data[10])),
+                new Vec3(double.Parse(data[3]), double.Parse(data[7]), double.Parse(data[11])),
+                new Vec3(double.Parse(data[4]), double.Parse(data[8]), double.Parse(data[12]))
+                );
         }
     }
 
@@ -127,17 +193,35 @@ namespace Frame3ddn
             TrapLoads = trapLoads;
         }
 
+        public static LoadCase Parse(string inputGravityString, List<NodeLoad> nodeLoads, List<UniformLoad> uniformLoads, List<TrapLoad> trapLoads)
+        {
+            string[] data = System.Text.RegularExpressions.Regex.Split(inputGravityString, @"\s{1,}");
+            return new LoadCase(
+                new Vec3(double.Parse(data[0]), double.Parse(data[1]), double.Parse(data[2])),
+                nodeLoads,
+                uniformLoads,
+                trapLoads
+            );
+        }
+
 
     }
 
     public class Input
     {
+        public bool IncludeShearDeformation { get; }
+        public bool IncludeGeometricStiffness { get; }
+        public int ExaggerateMeshDeformations { get; }
+        public int ZoomScale { get; }
+        public int XAxisIncrementForInternalForces { get; set; }
+
         public IReadOnlyList<Node> Nodes { get; }
         public IReadOnlyList<ReactionInput> ReactionInputs { get; }
         public IReadOnlyList<FrameElement> FrameElements { get; }
         public IReadOnlyList<LoadCase> LoadCases { get; }
 
-        public Input(IReadOnlyList<Node> nodes, IReadOnlyList<FrameElement> frameElements, IReadOnlyList<ReactionInput> reactionInputs, IReadOnlyList<LoadCase> loadCases)
+        public Input(IReadOnlyList<Node> nodes, IReadOnlyList<FrameElement> frameElements,
+            IReadOnlyList<ReactionInput> reactionInputs, IReadOnlyList<LoadCase> loadCases)
         {
             Nodes = nodes;
             FrameElements = frameElements;
@@ -145,6 +229,122 @@ namespace Frame3ddn
             LoadCases = loadCases;
         }
 
-        
+        /**
+         * Parse an input textfile to get Input object. The parsing logic is similar to the parsing methods in C program.
+         */
+        public static Input Parse(StreamReader sr)
+        {
+            List<Node> nodes = new List<Node>();
+            List<FrameElement> frameElements = new List<FrameElement>();
+            List<ReactionInput> reactionInputs = new List<ReactionInput>();
+            List<LoadCase> loadCases = new List<LoadCase>();
+            List<string> noComentInput = GetNoCommentInput(sr);
+            int currentLine = 0;
+
+            string title = noComentInput[currentLine++];
+
+            int nodeNum = int.Parse(noComentInput[currentLine++]);
+            for (int i = currentLine; currentLine < i + nodeNum; currentLine++)
+            {
+                nodes.Add(Node.Parse(noComentInput[currentLine]));
+            }
+
+            int reactionNodeNum = int.Parse(noComentInput[currentLine++]);
+            for (int i = currentLine; currentLine < i + reactionNodeNum; currentLine++)
+            {
+                reactionInputs.Add(ReactionInput.Parse(noComentInput[currentLine]));
+            }
+
+            int frameElementNum = int.Parse(noComentInput[currentLine++]);
+            for (int i = currentLine; currentLine < i + frameElementNum; currentLine++)
+            {
+                frameElements.Add(FrameElement.Parse(noComentInput[currentLine]));
+            }
+
+            bool IncludeShearDeformation = int.Parse(noComentInput[currentLine++]) != 0;
+            bool IncludeGeometricStiffness = int.Parse(noComentInput[currentLine++]) != 0;
+            double ExaggerateMeshDeformations = double.Parse(noComentInput[currentLine++]);
+            double ZoomScale = double.Parse(noComentInput[currentLine++]);
+            double XAxisIncrementForInternalForces = double.Parse(noComentInput[currentLine++]);
+
+            int LoadCaseNum = int.Parse(noComentInput[currentLine++]);
+            for (int i = 0; i < LoadCaseNum; i++)
+            {
+                string loadCaseGravityString = noComentInput[currentLine++];
+                int loadNodeNum = int.Parse(noComentInput[currentLine++]);
+                List<NodeLoad> nodeLoads = new List<NodeLoad>();
+                for (int j = currentLine; currentLine < j + loadNodeNum; currentLine++)
+                {
+                    nodeLoads.Add(NodeLoad.Parse(noComentInput[currentLine]));
+                }
+
+                int uniformLoadNum = int.Parse(noComentInput[currentLine++]);
+                List<UniformLoad> uniformLoads = new List<UniformLoad>();
+                for (int j = currentLine; currentLine < j + uniformLoadNum; currentLine++)
+                {
+                    uniformLoads.Add(UniformLoad.Parse(noComentInput[currentLine]));
+                }
+
+                int trapLoadNum = int.Parse(noComentInput[currentLine++]);
+                List<TrapLoad> trapLoads = new List<TrapLoad>();
+                for (int j = currentLine; currentLine < j + trapLoadNum; currentLine = currentLine + 3)
+                {
+                    string combinedData = noComentInput[currentLine] + " " + 
+                                          noComentInput[currentLine + 1] + " " +
+                                          noComentInput[currentLine + 2];
+                    trapLoads.Add(TrapLoad.Parse(noComentInput[currentLine]));
+                }
+
+                int internalConcentratedLoadNum = int.Parse(noComentInput[currentLine++]);
+                for (int j = currentLine; currentLine < j + internalConcentratedLoadNum; currentLine++)
+                {
+                }
+
+                int temperatureLoadNum = int.Parse(noComentInput[currentLine++]);
+                for (int j = currentLine; currentLine < j + temperatureLoadNum; currentLine++)
+                {
+                }
+
+                int prescribedDisplacementNum = int.Parse(noComentInput[currentLine++]);
+                for (int j = currentLine; currentLine < j + prescribedDisplacementNum; currentLine++)
+                {
+                }
+
+                LoadCase loadCase = LoadCase.Parse(loadCaseGravityString, nodeLoads, uniformLoads, trapLoads);
+                loadCases.Add(loadCase);
+            }
+            return new Input(nodes, frameElements, reactionInputs, loadCases);
+        }
+
+        private static List<string> GetNoCommentInput(StreamReader sr)
+        {
+            List<string> noComentInput = new List<string>();
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                line = line.Replace('\t', ' ');
+                if (string.IsNullOrEmpty(line)) //eliminate empty line
+                {
+                    continue;
+                }
+                else if (!line.Contains("#")) //save unchanged if there's no comment
+                {
+                    noComentInput.Add(line.Trim());
+                }
+                else //check if the line only contains comment
+                {
+                    string[] data = line.Split('#');
+                    if (string.IsNullOrEmpty(data[0])) //if it does, eliminate it
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        noComentInput.Add(data[0].Trim()); //otherwise, save the non-comment text only
+                    }
+                }
+            }
+            return noComentInput;
+        }
     }
 }
